4d73611be4b790a78c088782db0b1dfca71fcd86
park <park@DESKTOP-ED84OH8>
2021-05-06 17:40:21 +0900

    업캐스팅 다운캐스팅 다형성

---
+++ JavaEx/src/javaex/oop/casting/Cat.java
@@ -0,0 +1,10 @@
+package javaex.oop.casting;
+
+public class Cat extends Animal {
+	public Cat(String name) {
+		super(name);
+	}
+	public void meow() {
+		System.out.println(name + ": 야옹");
+	}
+}
---
+++ JavaEx/src/javaex/oop/casting/Dog.java
@@ -0,0 +1,11 @@
+package javaex.oop.casting;
+
+public class Dog extends Animal {
+	public Dog(String name) {
+		super(name);
+	}
+	
+	public void bark() {
+		System.out.println(name + ": 멍멍!");
+	}
+}
