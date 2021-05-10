dff36d6201b1907fa4278211216a046d2553c3c1
park <park@DESKTOP-ED84OH8>
2021-05-10 13:52:21 +0900

    Calendar, Generic

---
+++ JavaEx/src/com/javaex/api/date/DateEx.java
@@ -0,0 +1,79 @@
+package com.javaex.api.date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+public class DateEx {
+
+	public static void main(String[] args) {
+//		dateEx();
+		calendarEx();
+
+	}
+	
+	
+	private static void calendarEx() {
+		// 캘린더 불러오기
+		Calendar now = Calendar.getInstance();
+//		System.out.println("NOW:" + now);
+		Calendar custom = Calendar.getInstance();
+		custom.set(1999,11,31); // 1999년 12월 31일 (월 정보는 0부터 시작)
+		
+		// 정보 받아오기 .get(캘린더 상수)
+		System.out.printf("오늘은 %d년 %d월 %d일입니다.%n", now.get(Calendar.YEAR),now.get(Calendar.MONTH) + 1,now.get(Calendar.DATE));
+		
+		Calendar future = Calendar.getInstance();
+		future.add(Calendar.DATE, 100);
+		System.out.printf("100일 후 Future : %년 %월 %일\n",
+				future.get(Calendar.YEAR),
+				future.get(Calendar.MONTH) + 1,
+				future.get(Calendar.DATE));
+		
+		// 이 날은 무슨 요일?
+		int dow = future.get(Calendar.DAY_OF_WEEK);
+		System.out.println("100일 후 요일:" + dow);
+		
+		String dowStr = "?";
+		case Calendar.SUNDAY:
+			dowStr = "일요일";
+			break;
+		case Calendar.MONDAY:
+			dowStr = "월요일";
+			break;
+		case Calendar.TUESDAY:
+			dowStr = "화요일";
+			break;
+		case Calendar.WEDNESDAY:
+			dowStr = "수요일";
+			break;
+		case Calendar.THURSDAY:
+			dowStr = "목요일";
+			break;
+		case Calendar.FRIDAY:
+			dowStr = "금요일";
+			break;
+		case Calendar.SATURDAY:
+			dowStr = "토요일";
+	}
+	// 요일 정보 : SUNDAY(1) ~ SATURDAY (7)
+		System.out.println(dowStr;)
+	}
+	private static void dateEx() {
+		// 날짜 얻어오기
+		Date now = new Date();
+		
+		System.out.println(now);
+		
+		// 형식화 출력 :DateFormat 객체
+		DateFormat df = DateFormat.getDateInstance(DateFormat.FULL);
+		System.out.println("FULL:" + df.format(now));
+		df = DateFormat.getDateInstance(DateFormat.MEDIUM);
+		System.out.println("MEDIUM:" + df.format(now));
+		
+		// SimpleDateFormat = 포맷 문자열을 이용, 자유롭게 날짜 포맷
+		SimpleDateFormat sdf =
+				new SimpleDateFormat("yyyy년 MM월 dd일 HH시 mm분 ss초");
+		System.out.println("SDF:" + sdf.format(now));
+	}
+
+}
---
+++ JavaEx/src/com/javaex/api/generic/v2/BoxApp.java
@@ -0,0 +1,33 @@
+package com.javaex.api.generic.v2;
+
+public class BoxApp {
+
+	public static void main(String[] args) {
+		// Box 생성
+		Box<Integer> intBox = new Box<>();
+		intBox.setContent(2021); // intBox.setContent(Integer.valueOf(2021));
+//		intBox.setContent("Java");
+		
+		// 내용물을 꺼내 봅시다
+		int retVal = intBox.getContent(); // 타입 캐스팅의 번거로움 제거
+		System.out.println("내용물:" + retVal);
+		
+		// Box 생성 String 값을 설정
+		Box<String> strBox = new Box<>();
+		strBox.setContent("Java");
+		
+		String strVal = strBox.getContent();
+		System.out.println("strBox 내용물:" + strVal);
+		
+		// 만약에 캐스팅 잘못 했다면
+		// 컴파일러가 내부 데이터 타입을 확인해서 실행 이전에 미리 체크할 수 있다
+//		strVal = (String)intBox.getContent();
+		
+		// Generic을 사용하면
+		//	1 캐스팅을 해야 하는 불편함 해소.
+		// 2. 캐스팅 오류의 위험을 컴파일 타일에서 체크할 수 있다
+		
+
+	}
+
+}
