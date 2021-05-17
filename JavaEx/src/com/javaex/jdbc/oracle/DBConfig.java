0b9a652a6227957b60e5a3cafcb2689e397c4048
park <park@DESKTOP-ED84OH8>
2021-05-17 15:40:10 +0900

    JDBC Programming (Oracle)

---
+++ JavaEx/src/com/javaex/jdbc/oracle/DBConfig.java
@@ -0,0 +1,20 @@
+package com.javaex.jdbc.oracle;
+
+import java.sql.*;
+public class DBConfig {
+	public static String DBURL = "jdbc:oracle:thin:@localhost:1521:xe";
+	public static String DBUSER = "hr";
+	public static String DBPASS = "hr";
+	
+	Connection getConnection() throws SQLException {
+		Connection conn = null;
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection(DBURL, DBUSER, DBPASS);
+			} catch (ClassNotFoundException e) {
+				System.err.println("드라이버 로드 실패!");
+			}
+		return conn;
+	}
+}
